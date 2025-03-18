b928bcd3c3833d47593059f2db905ad3c698d19c
alifayyazmalik <alifayyaz@live.com>
2025-03-19 00:07:21 +0500

    Create constants.py

---
+++ src/params/constants.py
@@ -0,0 +1,4 @@
+# ★ TFM Parameters for Future Simulations ★
+GAMMA_CHEM = 1e12     # Chemical damping rate [s^-1]
+LAMBDA_BETA_SQ = 1e-5 # Wave-lump coupling [dimensionless]
+C_COHERENCE = 0.05    # Multi-atom coherence [eV]
